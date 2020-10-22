40318c3daa316babf721adf7f8e1b8075cd87648
Niklas Söderberg <nma.prob@gmail.com>
2020-10-22 18:35:47 +0200

    renamed a variable

--- main.js
+++ main.js
@@ -2,13 +2,13 @@
 var modal = document.getElementById("openModal");
 
 // Get the button that opens the modal
-var btn = document.getElementById("deleteEventBtn");
+var deleteEventBtn = document.getElementById("deleteEventBtn");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal 
-btn.onclick = function() {
+deleteEventBtn.onclick = function() {
   modal.style.display = "block";
 }
 
